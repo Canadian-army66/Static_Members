7a852deea88ea144f65f244aa3ff1adb89516dca
Canadian-army66 <142023388+Canadian-army66@users.noreply.github.com>
2024-04-24 09:21:08 -0500

    help me

---
+++ Static_Members_Kohnen_Christian/Calculate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Members_Kohnen_Christian
+{
+    static class Calculate
+    {
+        public static float result = 0.0f;
+        static Calculate()
+        {
+            result = 0.0f;
+        }
+
+        //intigers
+
+        public static int Add(int x, int y) //adding two methods
+        {
+            return x + y;
+        }
+        public static int Sub(int x, int y) //subtracting teo methods
+        {
+            return x - y;
+        }
+        public static int Mult(int x, int y) //multiplies two methods
+        {
+            return x * y;
+        }
+        public static int Div(int x, int y) //divides two methods
+        {
+            return x / y;
+        }
+
+        //floating
+
+        public static float Add(float x, float y) //adding two methods
+        {
+            return x + y;
+        }
+        public static float Sub(float x, float y) //subtracting teo methods
+        {
+            return x - y;
+        }
+        public static float Mult(float x, float y) //multiplies two methods
+        {
+            return x * y;
+        }
+        public static float Div(float x, float y) //divides two methods
+        {
+            return x / y;
+        }
+    }
+}
--- Static_Members_Kohnen_Christian/Program.cs
+++ Static_Members_Kohnen_Christian/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //intigers
+            Console.WriteLine("Intiger adition, subtraction, multiplication, and division of 92 and 42");
+            Console.WriteLine(Calculate.Add(92, 42));
+            Console.WriteLine(Calculate.Sub(92, 42));
+            Console.WriteLine(Calculate.Mult(92, 42));
+            Console.WriteLine(Calculate.Div(92, 42));
+
+            //floats
+            Console.WriteLine("float adition, subtraction, multiplication, and division of 92.45 and 42.88");
+            Console.WriteLine(Calculate.Add(92.45f, 42.88f));
+            Console.WriteLine(Calculate.Sub(92.45f, 42.88f));
+            Console.WriteLine(Calculate.Mult(92.45f, 42.88f));
+            Console.WriteLine(Calculate.Div(92.45f, 42.88f));
         }
     }
 }
